7968eff4211d9821a8d36b4b9d13a1549ff91191
Bo Motlagh <bo.motlagh@gmail.com>
2017-11-19 17:31:44 -0500

    finalizing product v 2.2.0

--- services/product/controller/product.js
+++ services/product/controller/product.js
@@ -3,7 +3,7 @@
  */
 
 import Promiseb from 'bluebird';
-
+import log from '../../../services/log/controller/log';
 const Product = Promiseb.promisifyAll(require('../model/product').default);
 import send from '../../callback';
 const Intent = Promiseb.promisifyAll(require('../../events/controller/event').default);
@@ -15,6 +15,7 @@ export default {
 			.catch(err => cb(send.failErr(err), null));
 	},
 	postProduct(options, cb) {
+		if(!options.private_code) options.private_code = uid(512);
 		const product = new Product(options);
 
 		product.save()
@@ -23,13 +24,17 @@ export default {
 	},
 	returnProductSlug(slug, active, cb) {
 		const query = {slug};
-		if(active!==null) query['active'] = active;
+		if(active) query['active'] = active;
 		Product.findOne(query)
 			.then(results => {
-				if(!results) cb(send.fail404('This product was not found'), null);
+				if(!results) {
+					return cb(send.fail404('This product was not found'), null);
+                }
 				return cb(null, send.success(results));
 			})
-			.catch(err => cb(send.failErr(err), null));
+			.catch((err) => {
+				return cb(send.failErr(err), null)
+            });
 	},
 	returnProduct(id, cb) {
 		Product.findOne({_id:id})
@@ -47,6 +52,19 @@ export default {
 			})
 			.catch(error => cb(send.failErr(error), null));
 	},
+    changeFirstUserUpdate(options) {
+		return new Promise((resolve, reject) => {
+			Product.findOneAndUpdate({ _id: options.id }, { enable_first_user: options.set }, { new: true })
+                .then(result => {
+                    if(!result) return reject(send.fail404('Product Not Found'));
+                    return resolve(send.success(result));
+                })
+                .catch(error => {
+                	log.error(error);
+                	return reject(send.failErr(error));
+                });
+		})
+	},
 	prepareIntent(event, cb) {
 		Intent.createIntentAsync(event)
 			.then(event => {
@@ -56,3 +74,19 @@ export default {
 			.catch(error => cb(send.failErr(error), null));
 	}
 };
+
+function uid (len) {
+    let buf = []
+        , chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        , charlen = chars.length;
+
+    for (let i = 0; i < len; ++i) {
+        buf.push(chars[getRandomInt(0, charlen - 1)]);
+    }
+
+    return buf.join('');
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
